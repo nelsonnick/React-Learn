1ba8220b99c9559cf9cfc02ec98417da11cf8ffb
徐汉琪 <nelsonnick@126.com>
2016-08-20 13:24:22 +0800

    完善左侧按钮

--- src/js/pages/UseCont.js
+++ src/js/pages/UseCont.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import UserTable from '../table/UserTable.js';
 export default class UseCont extends React.Component {
   render() {
     return (
       <div>
-        hello
+        <UserTable />
       </div>
     );
   }
